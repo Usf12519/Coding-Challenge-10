dc0382d197177260c6c5e78b4d5dc20ec5664df7
Cameron Smith <Cs11@usf.edu>
2024-10-20 23:29:59 -0400

    Create HTML structure for product page

---
+++ main.js
@@ -0,0 +1,34 @@
+// Task 1: Setup Product HTML Structure
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HTML Structure</title>
+<style>
+    Product {
+        border: 1px solid #cc;
+        padding: 30px;
+        max-width: 500px;
+    }
+
+    product h2 {
+        font-size 10px;
+        margin-bottom: 20px;
+    }
+
+    product p {
+        font-size 20px;
+        color: blue;  
+    }
+
+    product button {
+        margin-top: 5px;
+        padding: 5px;
+        width: 80%;
+        font-size: 20px;
+    }
+    </style>
+</head>
+</body>
